8d9e20fc0a3f2d3d352460f1d046b442c7c1db87
peters0908 <peters0908@gmail.com>
2022-11-14 16:23:07 +0800

    cache

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
+
 import private
-import requests
-print(private.secret.open_weather_key)
+import datasource
+
 
 def main():
     api_key = private.secret.open_weather_key
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q=Hualien,tw&APPID=75f99c6e5bf613675e743a9c3ab32009&lang=zh_tw&units=metric"
-
-    response = requests.get(url)
-    if response.ok :
-        print("下載成功")
-        allData = response.json()
-        city = allData['city']
-        print(city)
-
-
+    city = datasource.get_forcase_data("Taipei",api_key)
+    print(city)
+    
 if __name__ == "__main__":
     main()
