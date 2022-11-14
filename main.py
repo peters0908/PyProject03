ac96f3c1e340ea7c9fd02f4c8eb2ef7e46460dfd
peters0908 <peters0908@gmail.com>
2022-11-14 15:52:24 +0800

    main

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import private
+import requests
 print(private.secret.open_weather_key)
+
+def main():
+    api_key = private.secret.open_weather_key
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q=Hualien,tw&APPID=75f99c6e5bf613675e743a9c3ab32009&lang=zh_tw&units=metric"
+
+    response = requests.get(url)
+    if response.ok :
+        print("下載成功")
+        allData = response.json()
+        city = allData['city']
+        print(city)
+
+
+if __name__ == "__main__":
+    main()
